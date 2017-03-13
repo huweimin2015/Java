5c93e26bdf762898a86337e2fc6698d8a8ecc340
huweimin2015 <714747729@qq.com>
2017-03-13 15:48:31 +0800

    Add files via upload

--- sort.java
+++ sort.java
@@ -4,12 +4,33 @@
 输出：
 1 2 4 5 8
 要求：数组是通过sort程序的参数输入。
-*/
-public class sort{
-	 public static void main(String[] args){
-         //将args转换为int数组
-         
-         //对int数组排序后输出结果
-	 	 
-	}
-}
+*/                                 
+   import java.util.Arrays;                                        
+   public class sort{                                              
+	 public static void main(String[] args){                      
+		                                                            
+		 int[] sorted = new int[args.length];                       
+		                                                            
+		 //String数组转int数组                                       
+                                                 
+		 for(int i=0;i<args.length;i++){                                     
+			   sorted[i] = Integer.parseInt(args[i]);  
+     }
+		 
+		
+		 //冒泡排序                                                 
+		 for(int i = 0; i < sorted.length-1; i++){                      
+			 for(int j = 0; j < sorted.length-1-i; j++){              
+				 if(sorted[j] > sorted[j+1]){                           
+					 int temp = sorted[j];                                
+					 sorted[j] = sorted[j+1];                             
+					 sorted[j+1] = temp;                                  
+				 }                                                      
+			 }                                                        
+		 }                                                          
+	 	
+	 	 for(int i=0;i<sorted.length;i++){                                                           
+		   System.out.print(sorted[i]);
+		}                                                  
+	}                                                             
+}                                                               
